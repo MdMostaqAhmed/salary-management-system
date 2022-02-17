485205a2d0c9bf4b814afa88f9e23a7f8f02fc63
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-02-17 19:08:14 +0600

    percentage count done

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-const foodCost = document.getElementById("food-cost");
-const rentCost = document.getElementById("rent-cost");
-const clothCost = document.getElementById("cloth-cost");
+function totalExpenses() {
+  const foodCost = document.getElementById("food-cost");
+  const rentCost = document.getElementById("rent-cost");
+  const clothCost = document.getElementById("cloth-cost");
+  const total =
+    parseInt(foodCost.value) +
+    parseInt(rentCost.value) +
+    parseInt(clothCost.value);
 
-const balanceCalculate = document
-  .getElementById("calculate")
-  .addEventListener("click", function () {
-    const total =
-      parseInt(foodCost.value) +
-      parseInt(rentCost.value) +
-      parseInt(clothCost.value);
+  return total;
+}
 
-    // Total Expenses
-    const expenses = (document.getElementById("expenses").innerText = total);
-    //Balance
+function balance() {
+  const total = totalExpenses();
+  const expenses = (document.getElementById("expenses").innerText = total);
+   const incomeText = document.getElementById("income");
+   const income = parseInt(incomeText.value);
+   const balance = (document.getElementById("balance").innerText = income - expenses);
+   return balance;
+   }
 
-    const incomeText = document.getElementById("income");
-    const income = parseInt(incomeText.value);
-
-    const balance = (document.getElementById("balance").innerText =
-      income - expenses);
+const balanceCalculate = document.getElementById("calculate").addEventListener("click", function () {
+    balance()
   });
+
+
+function savingAmount() {
+  const percentageText = document.getElementById("percentage").value;
+  const percentage = parseInt(percentageText);
+  const saving = ( document.getElementById("saving-amount").innerText = (balance() * percentage) / 100);
+  return saving;
+}
+
+function remainingBalance() {
+  document.getElementById('remaining-balance').innerText = balance() - savingAmount();
+  console.log();
+}
+
+
+ 
+document.getElementById("saving").addEventListener("click", function () {
+savingAmount();
+remainingBalance();
+});
