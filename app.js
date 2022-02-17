3c92096ff7c5bb8292f6709e8f1aeb72d12f1408
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-02-17 22:00:54 +0600

    error handling done

--- app.js
+++ app.js
@@ -1,44 +1,76 @@
+// get all input value
 function totalExpenses() {
   const foodCost = document.getElementById("food-cost");
   const rentCost = document.getElementById("rent-cost");
   const clothCost = document.getElementById("cloth-cost");
-  const total =
+
+  const incomeText = document.getElementById("income");
+  let income = parseInt(incomeText.value);
+  
+  let total =
     parseInt(foodCost.value) +
     parseInt(rentCost.value) +
     parseInt(clothCost.value);
 
-  return total;
+    if (income < total) {
+      return alert('Please enter valid number'), total = 0, income = 0;
+    }
+
+    else if ( income > total ){
+      return total;
+    }
 }
 
+// get balance 
 function balance() {
   const total = totalExpenses();
   const expenses = (document.getElementById("expenses").innerText = total);
-   const incomeText = document.getElementById("income");
-   const income = parseInt(incomeText.value);
-   const balance = (document.getElementById("balance").innerText = income - expenses);
-   return balance;
+  const incomeText = document.getElementById("income");
+  const income = parseInt(incomeText.value);
+  const balance = (document.getElementById("balance").innerText = income - expenses);
+  
+    return balance;
+ 
    }
 
+   // handle calculate button
 const balanceCalculate = document.getElementById("calculate").addEventListener("click", function () {
-    balance()
+   balance() 
+     
   });
 
-
+//get saving amount
 function savingAmount() {
   const percentageText = document.getElementById("percentage").value;
   const percentage = parseInt(percentageText);
-  const saving = ( document.getElementById("saving-amount").innerText = (balance() * percentage) / 100);
-  return saving;
-}
+  let saving = ( document.getElementById("saving-amount").innerText = (balance() * percentage) / 100);
+  if (saving < 0) {
+    return alert('Please enter positive number'), saving = 0;
+  }
+  else if (saving > 0) {
+    return saving
+  }
+};
 
+//get remaining balance
 function remainingBalance() {
-  document.getElementById('remaining-balance').innerText = balance() - savingAmount();
-  console.log();
-}
+ let remainingBalance = document.getElementById('remaining-balance').innerText = balance() - savingAmount();
+
+ if (remainingBalance < 0) {
+   return alert('Please enter valid percentage amount'), remainingBalance = (document.getElementById('remaining-balance').innerText = balance()), saving = 0;
+ } else if (remainingBalance > 0 ){
+   return remainingBalance
+ }
+};
 
 
- 
+ //handle save button
 document.getElementById("saving").addEventListener("click", function () {
-savingAmount();
-remainingBalance();
+  if(savingAmount() < 0 && remainingBalance() < 0){
+    return alert("Please enter positive number")
+  } else if (savingAmount() > 0){
+    savingAmount();
+    remainingBalance()
+  }
+
 });
